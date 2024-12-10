8882b5a2ae49ffdf750cc7485ad026fc6ffa87e1
Germano Rizzo <oss@germanorizzo.it>
2024-12-10 17:10:00 +0100

    v0.1.2 - fixes to cookie management

--- caddy_knockknock.go
+++ caddy_knockknock.go
@@ -12,7 +12,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const VERSION = "v0.1.0"
+const VERSION = "v0.1.2"
 
 var cookey string = "kkkey_" + genRandomString(4)
 
@@ -52,27 +52,38 @@ func cutToColon(input string) string {
 
 func (m CaddyKnockKnock) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	var ip = cutToColon(r.RemoteAddr)
-	var key = r.URL.Query().Get("kkkey")
 
-	if key != "" {
+	var cookieSituation = false // false: not present; true: present and invalid
+	cookie, err := r.Cookie(cookey)
+	if err != nil {
+		return caddyhttp.Error(505, err)
+	}
+	if cookie != nil {
+		if cookie.Value == getSession(ip) {
+			return next.ServeHTTP(w, r)
+		}
+		cookieSituation = true
+	}
+
+	key := r.URL.Query().Get("kkkey")
+	if key == "" { // key not specified
+		if cookieSituation { // cookie present and invalid
+			return caddyhttp.Error(403, errors.New("invalid session cookie"))
+		} else { // cookie absent
+			return caddyhttp.Error(403, errors.New("both URL (kk)key and session cookie are missing"))
+		}
+	} else { // key is present, maybe valid
 		q := r.URL.Query()
 		q.Del("kkkey")
 		r.URL.RawQuery = q.Encode()
 
-		if ok, _ := comparePasswordAndHash(key, m.HashedKey); !ok {
-			return caddyhttp.Error(403, errors.New("wrong key"))
-		}
-	}
-
-	cookie, _ := r.Cookie(cookey)
-	if cookie != nil {
-		if cookie.Value != getSession(ip) {
-			return caddyhttp.Error(403, errors.New("wrong cookie"))
-		}
-	} else {
-		if key == "" {
-			return caddyhttp.Error(403, errors.New("key or cookie must be provided"))
+		if ok, err := comparePasswordAndHash(key, m.HashedKey); err != nil {
+			return caddyhttp.Error(505, err)
+		} else if !ok {
+			// key is invalid, always fail
+			return caddyhttp.Error(403, errors.New("invalid URL (kk)key"))
 		} else {
+			// key valid. In this branch, the cookie is missing or invalid, let's set it
 			http.SetCookie(w, &http.Cookie{
 				Name:     cookey,
 				Value:    newSession(ip),
@@ -80,10 +91,9 @@ func (m CaddyKnockKnock) ServeHTTP(w http.ResponseWriter, r *http.Request, next 
 				HttpOnly: true,
 				Secure:   true,
 			})
+			return next.ServeHTTP(w, r)
 		}
 	}
-
-	return next.ServeHTTP(w, r)
 }
 
 func (m *CaddyKnockKnock) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
